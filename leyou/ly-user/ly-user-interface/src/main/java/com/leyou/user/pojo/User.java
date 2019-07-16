8e3a0c7beea59e3bce0dcb4b74ba37708fd6ecd9
cherry-cheng <chengyonghui@yizhanit.com>
2019-07-16 18:32:58 +0800

    hibernate validator

--- leyou/ly-user/ly-user-interface/src/main/java/com/leyou/user/pojo/User.java
+++ leyou/ly-user/ly-user-interface/src/main/java/com/leyou/user/pojo/User.java
@@ -2,9 +2,11 @@ package com.leyou.user.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import tk.mybatis.mapper.annotation.KeySql;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Table(name = "tb_user")
@@ -14,8 +16,11 @@ public class User {
     @KeySql(useGeneratedKeys = true)
     private Long id;
 
+    @NotEmpty(message = "用户名不能为空")
+    @Length(min = 4, max = 32, message = "用户名长度必须在4~32位")
     private String username;// 用户名
 
+    @Length(min = 4, max = 32, message = "密码长度必须在4~32位")
     @JsonIgnore
     private String password;// 密码
 
