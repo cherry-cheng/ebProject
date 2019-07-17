0ac1dbc53f13fae04d6c35d34da14ee5cb69eeef
cherry-cheng <chengyonghui@yizhanit.com>
2019-07-17 19:05:23 +0800

    验证verify用户接口

--- leyou/ly-auth-center/ly-auth-service/src/main/java/com/leyou/auth/web/AuthController.java
+++ leyou/ly-auth-center/ly-auth-service/src/main/java/com/leyou/auth/web/AuthController.java
@@ -1,20 +1,30 @@
 package com.leyou.auth.web;
 
+import com.leyou.auth.config.JwtProperties;
+import com.leyou.auth.entity.UserInfo;
 import com.leyou.auth.service.AuthService;
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
 import com.leyou.common.utils.CookieUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@EnableConfigurationProperties(JwtProperties.class)
 public class AuthController {
+
+    @Autowired
+    private JwtProperties prop;
+
     @Autowired
     private AuthService authService;
 
@@ -36,5 +46,23 @@ public class AuthController {
         // TODO 写入cookie
         CookieUtils.setCookie(request, response, cookieName, token);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("verify")
+    public ResponseEntity<UserInfo> verify(@CookieValue("LY_TOKEN") String token) {
+        if (StringUtils.isBlank(token)) {
+            // 如果没有token,证明未登录，返回403
+            throw new LyException(ExceptionEnum.UNAUTHORIZED);
+        }
+        try {
+            // 解析token
+            UserInfo info = JwtUtils.getInfoFromToken(token, prop.getPublicKey());
+
+            // 已登录，返回用户信息
+            return ResponseEntity.ok(info);
+        } catch (Exception e) {
+            // token已过期，或者  token被篡改
+            throw new LyException(ExceptionEnum.UNAUTHORIZED);
+        }
     }
 }
