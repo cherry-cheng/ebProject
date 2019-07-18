864533fba2260b8c53829ac48187cac0c686c035
cherry-cheng <chengyonghui@yizhanit.com>
2019-07-18 16:00:27 +0800

    购物车的redis实现

---
+++ leyou/ly-cart/src/main/java/com/leyou/cart/interceptors/UserInterceptor.java
@@ -0,0 +1,73 @@
+package com.leyou.cart.interceptors;
+
+import com.leyou.auth.entity.UserInfo;
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.cart.config.JwtProperties;
+import com.leyou.common.utils.CookieUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class UserInterceptor implements HandlerInterceptor {
+
+    private JwtProperties prop;
+
+    private static final ThreadLocal<UserInfo> tl = new ThreadLocal<>();
+
+    public UserInterceptor(JwtProperties prop) {
+        this.prop = prop;
+    }
+
+    /**
+     * return true  放行
+     * returnn false 拦截
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 获取cookie中的token
+        String token = CookieUtils.getCookieValue(request, prop.getCookieName());
+        try {
+            // 解析token
+            UserInfo user = JwtUtils.getInfoFromToken(token, prop.getPublicKey());
+            // 传递user
+//            request.setAttribute("user", user);
+            // 使用线程域  存数据  k  v结构  key是线程id
+            tl.set(user);
+            // 放行
+            return true;
+        } catch (Exception e) {
+            log.error("[购物车服务] 解析用户身份失败。", e);
+            return false;
+        }
+    }
+
+    /**
+     * 视图渲染完毕才执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        // 最后用完数据，一定要清空
+        tl.remove();
+    }
+
+    /**
+     * 取用户信息
+     * @return
+     */
+    public static UserInfo getUser() {
+        return tl.get();
+    }
+}
